9d9315555623eec789f1a68cbc9bab8a1691720f
kibetkoech <vickibelangs@gmail.com>
2025-10-01 17:28:22 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+// 1. Declare customerName in the global scope
+var customerName = 'bob';
+
+// 2. A function that modifies the global variable customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// 3. Declare bestCustomer in global scope and set it inside a function
+var bestCustomer;
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// 4. Overwrite the best customer
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// 5. leastFavoriteCustomer should never be reassigned (const)
+const leastFavoriteCustomer = 'some initial value';
+function changeLeastFavoriteCustomer() {
+  // This should cause an error if reassigned, which the test expects
+  leastFavoriteCustomer = 'new customer';
+}
+
